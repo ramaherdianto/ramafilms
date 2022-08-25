dad896bfa9153845852e069e88ba8378489eff3d
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-08-25 16:45:37 +0700

    card superhero

--- src/components/Superhero.jsx
+++ src/components/Superhero.jsx
@@ -1,29 +1,79 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
 import antmanImage from "../assets/images/superhero/antman.jpg";
+import avengerImage from "../assets/images/superhero/avenger.jpg";
+import batmanImage from "../assets/images/superhero/batman.jpg";
+import robinhoodImage from "../assets/images/superhero/robinhood.jpg";
+import spidermanImage from "../assets/images/superhero/spiderman-cover.jpg";
+import supermanImage from "../assets/images/superhero/superman.jpg";
 
 const Superhero = () => {
-  const superheroMovies = [
-    {
-      srcGambar: "../assets/images/superhero/antman.jpg",
-      movieName: "Antman",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
-    },
-  ];
-
   return (
     <div>
-      {/* <img src={antmanImage} alt="" srcset="" /> */}
-      {superheroMovies.map((superheroMovie) => {
-        return (
-          <div>
-            <Container>
-              <img src={superheroMovie.srcGambar} alt="" />
-              <h1>{superheroMovie.movieName}</h1>
-              <p>{superheroMovie.description}</p>
-            </Container>
-          </div>
-        );
-      })}
+      <Container>
+        <h1 className="text-center mb-5">Superhero</h1>
+        <Row>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage">
+              <Image src={antmanImage} alt="Dune Movies" className="images" />
+              <div className="bg-dark p-3">
+                <Card.Title className="text-center">ANTMAN</Card.Title>
+                <Card.Text className="text-left">This is a wider card with natural lead-in to additional content</Card.Text>
+                <Card.Text className="text-left">Last updated 5 mins ago</Card.Text>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage">
+              <Image src={avengerImage} alt="Dune Movies" className="images" />
+              <div className="bg-dark p-3">
+                <Card.Title className="text-center">AVENGERS</Card.Title>
+                <Card.Text className="text-left">This is a wider card with natural lead-in to additional content</Card.Text>
+                <Card.Text className="text-left">Last updated 5 mins ago</Card.Text>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage">
+              <Image src={batmanImage} alt="Dune Movies" className="images" />
+              <div className="bg-dark p-3">
+                <Card.Title className="text-center">BATMAN</Card.Title>
+                <Card.Text className="text-left">This is a wider card with natural lead-in to additional content</Card.Text>
+                <Card.Text className="text-left">Last updated 5 mins ago</Card.Text>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage">
+              <Image src={robinhoodImage} alt="Dune Movies" className="images" />
+              <div className="bg-dark p-3">
+                <Card.Title className="text-center">ROBIN HOOD</Card.Title>
+                <Card.Text className="text-left">This is a wider card with natural lead-in to additional content</Card.Text>
+                <Card.Text className="text-left">Last updated 5 mins ago</Card.Text>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage">
+              <Image src={spidermanImage} alt="Dune Movies" className="images" />
+              <div className="bg-dark p-3">
+                <Card.Title className="text-center">SPIDERMAN</Card.Title>
+                <Card.Text className="text-left">This is a wider card with natural lead-in to additional content</Card.Text>
+                <Card.Text className="text-left">Last updated 5 mins ago</Card.Text>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage">
+              <Image src={supermanImage} alt="Dune Movies" className="images" />
+              <div className="bg-dark p-3">
+                <Card.Title className="text-center">SUPERMAN</Card.Title>
+                <Card.Text className="text-left">This is a wider card with natural lead-in to additional content</Card.Text>
+                <Card.Text className="text-left">Last updated 5 mins ago</Card.Text>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
