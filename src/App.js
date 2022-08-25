86b4d0fb79c42dd065cd9751ce8b898e74b7ccb1
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-08-25 17:08:44 +0700

    fix: fix loopcard movie

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import NavigationBar from "./components/NavigationBar";
 import Superhero from "./components/Superhero";
 import "./style/Page.css";
 function App() {
-   return (
-      <div>
-         <div className="myBg border">
-            <NavigationBar />
-            <Intro />
-         </div>
-
-         <div className="section-Superhero" id="superhero">
-            <Superhero />
-         </div>
+  return (
+    <div>
+      <div className="myBg">
+        <NavigationBar />
+        <Intro />
       </div>
-   );
+
+      <div className="section-Superhero" id="superhero">
+        <Superhero />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Superhero.jsx
+++ src/components/Superhero.jsx
@@ -1,31 +1,68 @@
-import { Container } from "react-bootstrap";
+import { Container, Card, Col, Row, Image } from "react-bootstrap";
 import antmanImage from "../assets/images/superhero/antman.jpg";
+import avengerImage from "../assets/images/superhero/avenger.jpg";
+import batmanImage from "../assets/images/superhero/batman.jpg";
+import robinhoodImage from "../assets/images/superhero/robinhood.jpg";
+import spidermanImage from "../assets/images/superhero/spiderman-cover.jpg";
+import supermanImage from "../assets/images/superhero/superman.jpg";
 
 const Superhero = () => {
-   const superheroMovies = [
-      {
-         srcGambar: antmanImage,
-         movieName: "Antman",
-         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
-      },
-   ];
+  const superheroMovies = [
+    {
+      srcGambar: antmanImage,
+      movieName: "ANTMAN",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
+    },
+    {
+      srcGambar: avengerImage,
+      movieName: "AVENGERS",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
+    },
+    {
+      srcGambar: batmanImage,
+      movieName: "BATMAN",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
+    },
+    {
+      srcGambar: robinhoodImage,
+      movieName: "ROBIN HOOD",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
+    },
+    {
+      srcGambar: spidermanImage,
+      movieName: "SPIDERMAN",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
+    },
+    {
+      srcGambar: supermanImage,
+      movieName: "SUPERMAN",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto asperiores doloribus, dolores a vitae laborum?",
+    },
+  ];
 
-   return (
-      <div>
-         {/* <img src={antmanImage} alt="" srcset="" /> */}
-         {superheroMovies.map((superheroMovie) => {
+  return (
+    <div>
+      <Container>
+        <h1 className="text-center mb-5">Superhero</h1>
+        <Row>
+          {superheroMovies.map((superheroMovie) => {
             return (
-               <div>
-                  <Container>
-                     <img src={superheroMovie.srcGambar} alt="" />
-                     <h1>{superheroMovie.movieName}</h1>
-                     <p>{superheroMovie.description}</p>
-                  </Container>
-               </div>
+              <Col md={4} className="movieWrapper" id="superhero">
+                <Card className="movieImage">
+                  <Image src={superheroMovie.srcGambar} alt="Dune Movies" className="images" />
+                  <div className="bg-dark p-3">
+                    <Card.Title className="text-center">{superheroMovie.movieName}</Card.Title>
+                    <Card.Text className="text-left">{superheroMovie.description}</Card.Text>
+                    <Card.Text className="text-left">Last updated 5 mins ago</Card.Text>
+                  </div>
+                </Card>
+              </Col>
             );
-         })}
-      </div>
-   );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Superhero;
